b4cbe7b46e38730f2c391f638f14c7953b13a521
melbahy <Sha8fweba6>
2019-07-29 11:50:20 +0200

    ms codepush trail

--- usersList.js
+++ usersList.js
@@ -21,7 +21,7 @@ class UsersList extends Component{
     renderUsers() {
         return this.state.users.map((user, i) =>
             <TouchableOpacity onPress={()=>this.pressedCard(user)} key={i}>
-                <Card title={user.login}>
+                <Card title={"hello"}>
                     <View  style={{alignItems: "center"}}>
                         <Image source={{uri: user.avatar_url}} style={{width: 300, height: 300, borderRadius: 150}}/>
                     </View>
